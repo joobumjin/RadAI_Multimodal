fix: sparse modalities with masking instead of Nones

data/mmap_data.py
@@ -322,7 +322,7 @@ class MemmapDatasetMultimodal(Dataset):
             for (bin_name, bin_ind), k, length in zip(self.modality_inds.items(), list(ks), list(lengths)):
                 if self.sparse and length == 0:
                     sample[f"{bin_name} mask"] = 0
-                    sample[bin_name] = np.zeros((self._feat_dim))
+                    sample[bin_name] = np.zeros((self._feat_dim), dtype=self._dtype_str)
                 else:
                     sample[f"{bin_name} mask"] = 1
                     start = np.random.randint(0, length - k + 1) if k < length else 0