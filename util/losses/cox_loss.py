77b88cd50a88d5e25b0b285ec3b46b3b8e517e48
joobumjin <bumjin.joo@gmail.com>
2026-06-27 18:09:07 -0400

    fix: censoring

--- util/losses/cox_loss.py
+++ util/losses/cox_loss.py
@@ -9,7 +9,7 @@ def cox_nll_loss(preds, x):
         - "survival_right_censor": per patient surviving or not
         - "survival_days": survival in days
     """
-    surviving = x["survival_right_censor"].squeeze(-1).bool()
+    surviving = ~(x["survival_right_censor"].squeeze(-1).bool())
     time = x["survival_days"].squeeze(-1)
 
     return neg_partial_log_likelihood(log_hz=preds, event=surviving, time=time)
